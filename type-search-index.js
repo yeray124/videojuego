15eeb84603f25c856688628e56dfa6c5becaf532
yeray124 <yeray0451@gmail.com>
2024-04-04 20:21:40 +0200

    Documentacion
    generar Java doc

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Enemigo"},{"p":"<Unnamed>","l":"Inventario"},{"p":"<Unnamed>","l":"Jugador"},{"p":"<Unnamed>","l":"Main"}];updateSearchResults();
